ecbd35e9c4563c0e4b536cbe0049f580552df827
Abhay Gupta <abhaygupta82764@gmail.com>
2023-06-09 22:11:14 +0530

    code added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj=new RWOnly();
+//      obj.name="Aman";
+//      System.out.println(obj.name);
+      obj.setName("Aman");
+      obj.getName();
+  }
 }
